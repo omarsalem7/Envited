a43ea6e6e829c7a469732cf365e00dec3101473b
Omar Salem <omarsalem0721@gmail.com>
2022-10-03 10:17:47 +0200

    Organize file structure by routes and add routes foreach page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './error/errorPage';
+import Event from './event/event';
+import Create from './create/create';
+import Landing from './landing/landing';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/event',
+    element: <Event />,
+  },
+  {
+    path: '/create',
+    element: <Create />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
